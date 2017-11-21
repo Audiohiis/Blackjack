026f3cd3509aab2d7800dadaa8a32ee7bb24b0cf
Hedi Tamm <hedi.tamm@bigbank.ee>
2017-11-21 12:45:59 +0200

    Create class for cards

--- Blackjack/Blackjack.Client.Console/Program.cs
+++ Blackjack/Blackjack.Client.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Blackjack.Client.Console
 {
     using System;
+    using Blackjack.Core;
 
     class Program
     {
@@ -11,25 +12,44 @@
 
             Console.WriteLine("Welcome to the game of Blackjack");
 
-            Console.WriteLine("You have been dealt: 4C, 6H");
-            Console.WriteLine("House have been dealt: 8S, [?]");
+            var c4 = new Card("4", 'C', 4);
+            c4.Hidden = false;
+            var h6 = new Card("6", 'H', 6);
+            h6.Hidden = false;
+            var s8 = new Card("8", 'S', 8);
+            s8.Hidden = false;
+            var dJ = new Card("J", 'D', 10);
+            dJ.Hidden = false;
+
+            Console.WriteLine($"You have been dealt: { GetCardDescription(c4) }, { GetCardDescription(h6) }");
+            Console.WriteLine($"House have been dealt: { GetCardDescription(s8) }, { GetCardDescription(dJ) }");
 
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("Choose: 1 - Take another card");
             Console.WriteLine("Choose: 2 - To finish");
             Console.WriteLine("I choose: 1");
 
-            Console.WriteLine("You have been dealt: 4S");
-            Console.WriteLine("House have been dealt: [?]");
+            var s4 = new Card("4", 'S', 4);
+            s4.Hidden = false;
+            var hI = new Card("I", 'H', 10);
+            hI.Hidden = false;
+
+            Console.WriteLine($"You have been dealt: { GetCardDescription(s4) }");
+            Console.WriteLine($"House have been dealt: { GetCardDescription(hI) }");
 
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("Choose: 1 - Take another card");
             Console.WriteLine("Choose: 2 - To finish");
             Console.WriteLine("I choose: 2");
 
-            Console.WriteLine("Your sum: ");
-            Console.WriteLine("House sum: ");
+            Console.WriteLine("Your sum: 14");
+            Console.WriteLine("House sum: 17");
             Console.WriteLine("House won!");
+        }
+
+        public static string GetCardDescription(Card card)
+        {
+            return card.Hidden ? "[?]" : card.Description;
         }
     }
 }
---
+++ Blackjack/Blackjack.Core/Card.cs
@@ -0,0 +1,23 @@
+﻿namespace Blackjack.Core
+{
+    using System;
+
+    public class Card
+    {
+        // Kaardi klass: mast, number, punktid, nähtav või mitte
+        // Diamonds (♦), Clubs (♣), Hearts (♥) and Spades (♠)
+
+        private string _rank { get; }
+        private char _suite { get; }
+        private int _points { get; }
+        public string Description => _rank + _suite; // it's an expression body
+        public Boolean Hidden { get; set; } = true;
+
+        public Card(string rank, char suite, int points)
+        {
+            this._rank = rank;
+            this._suite = suite;
+            this._points = points;
+        }
+    }
+}
