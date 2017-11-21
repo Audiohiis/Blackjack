a316f808c78f1fa9fafcda1b63903ee44e74d0cf
Hedi Tamm <hedi.tamm@bigbank.ee>
2017-11-21 10:31:31 +0200

    Initial commit

---
+++ Blackjack/Blackjack.Client.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blackjack.Client.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
